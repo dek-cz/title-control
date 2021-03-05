f697b35754ffd55f107a990c91fddd9b27c47ece
Záruba Jan <jan.zaruba@dek-cz.com>
2021-03-05 19:40:20 +0100

    fix tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
+
 $p1 = __DIR__ . '/../vendor/autoload.php';
 $p2 = __DIR__ . '/../../../autoload.php';
 file_exists($p1) ? (require $p1) : (require $p2);
